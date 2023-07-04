acb69ece7e5ddf6e5cdd56178de246d0924455c9
Hygor Marques <hygor.marques@protonmail.com>
2023-07-04 09:03:04 -0300

    Fixed the customer list

--- connection/connection.php
+++ connection/connection.php
@@ -1,7 +1,7 @@
 <?php
     $hostdb = "localhost";
-    $userdb = "root";  // You must add this user to your local database or change for 
-    $passdb = "1413Hyg";  // some user that exists already and have all permissions
+    $userdb = "admin";  // You must add this user to your local database or change for 
+    $passdb = "admin";  // some user that exists already and have all permissions
     $dbname = "pessoal";
 
     $conn = mysqli_connect($hostdb, $userdb, $passdb, $dbname);
--- pages/customer.php
+++ pages/customer.php
@@ -11,23 +11,21 @@
         <td><strong>Phone</strong></td>
         <td><strong>Address</strong></td>
         <td><strong>Registration Date</strong></td>
+    <?php
+        $result = $conn->query("SELECT * FROM customers");
+        while($row_usuario = mysqli_fetch_assoc($result)){
+            echo "<tr>";
+            echo "  <td>". $row_usuario['Customer_ID']. "</td>";
+            echo "  <td>". $row_usuario['Customer_Name']. "</td>";
+            echo "  <td>". $row_usuario['Customer_CPF']. "</td>";
+            echo "  <td>". $row_usuario['Customer_Phone']. "</td>";
+            echo "  <td>". $row_usuario['Customer_Address']. "</td>";
+            echo "  <td>". $row_usuario['Customer_Registration_Date']. "</td>";
+            echo "</tr>";
+        }
+    ?>
     </table>
     <button><a href="customerCreate.php" style="text-decoration: none; color: black;">Create Customer</a></button>
     <button><a href="index.php" style="text-decoration: none; color: black;">Back</a></button>
-    <?php
-        $result_usuarios = "SELECT * FROM custormers";
-        $resultado_usuarios = mysqli_query($conn, $result_usuarios);
-        echo $resultado_usuarios;
-        // while($row_usuario = mysqli_fetch_assoc($resultado_usuarios)){
-        //     // echo "<tr>";
-        //     echo "ID: ". $row_usuario['Customer_ID']. "<br>";
-        //     // echo "  <td>". $row_usuario['Customer_Name']. "</td>";
-        //     // echo "  <td>". $row_usuario['Customer_CPF']. "</td>";
-        //     // echo "  <td>". $row_usuario['Customer_Phone']. "</td>";
-        //     // echo "  <td>". $row_usuario['Customer_Address']. "</td>";
-        //     // echo "  <td>". $row_usuario['Customer_Registration_Date']. "</td>";
-        //     // echo "</tr>";
-        // }
-    ?>
     
 <?php require "../structure/footer.php"?>
