a59dc70dc56d5e494d9e8dfd9fa8ff0378664bcf
Hygor Marques <hygor.marques@protonmail.com>
2023-07-06 17:29:38 -0300

    The parking function now shows only the vehicles that have not yet been parked.

--- pages/parkingCreate.php
+++ pages/parkingCreate.php
@@ -12,48 +12,73 @@
                     </thead>
                     <tbody>
                         <tr>
-                            <td><input type="radio" name="parking_space" value="1" required>1</td>
-                            <td>Parking Space</td>
+                            <td><input type="radio" name="parking_space" value="1" required></td>
+                            <td>Parking Space 1</td>
                         </tr>
                         <tr>
-                            <td><input type="radio" name="parking_space" value="2" required>2</td>
-                            <td>Parking Space</td>
+                            <td><input type="radio" name="parking_space" value="2" required></td>
+                            <td>Parking Space 2</td>
                         </tr>
                         <tr>
-                            <td><input type="radio" name="parking_space" value="3" required>3</td>
-                            <td>Parking Space</td>
+                            <td><input type="radio" name="parking_space" value="3" required></td>
+                            <td>Parking Space 3</td>
                         </tr>
                     </tbody>
-                </table>
-                <table>
-                    <thead>
-                        <legend>Registered Vehicles</legend>
-                        <th>#</th>
-                        <th>Vehicle</th>
-                        <th>Customer</th>
-                    </thead>
-                    <tbody>
-                        <?php
-                            $result = $conn->query("SELECT * FROM vehicles");
-                            while($row_vehicle = mysqli_fetch_assoc($result)){
-                                echo "<tr onclick=lineSelect(this)>";
-                                echo "  <input type='hidden' value=".$row_vehicle['Customer_ID']." name='customer_id'>";
-                                echo "  <td><input type='radio' name='vehicle_id' value=".$row_vehicle['Vehicle_ID']." required>". $row_vehicle['Vehicle_ID']. "</td>";
-                                echo "  <td>". $row_vehicle['Vehicle_Desc']. "</td>";
-                                echo "  <td>". $row_vehicle['Customer_ID']. "</td>";
-                                echo "</tr>";
-                            }
-                        ?>
-                    </tbody>
-                    <script>
-                        let customer = null;
+                </table><br>
+                <div id="vehicles">
+                    <table>
+                        <thead>
+                            <strong><legend>Available Vehicles</legend></strong><br>
+                            <th>Vehicle</th>
+                            <th>Plate</th>
+                            <th>#</th>
+                        </thead>
+                        <tbody>
+                            <?php
+                                $result = $conn->query("SELECT * FROM vehicles WHERE Vehicle_ID 
+                                NOT IN (SELECT Vehicle_ID FROM parking);");
+                                while($row_vehicle = mysqli_fetch_assoc($result)){
+                                    echo "<tr onclick=lineSelect(this)>";
+                                    echo "  <input type='hidden' value=".$row_vehicle['Customer_ID']." name='customer_id'>";
+                                    echo "  <td>". $row_vehicle['Vehicle_Desc']. "</td>";
+                                    echo "  <td>". $row_vehicle['Vehicle_Plate']. "</td>";
+                                    echo "  <td><input type='radio' name='vehicle_id' value=".$row_vehicle['Vehicle_ID']." required></td>";
+                                    echo "</tr>";
+                                }
+                            ?>
+                        </tbody>
+                        <script>
+                            let customer = null;
 
-                        function lineSelect(line){
-                            customer = line.querySelector('input[name="customer_id"]').value;
-                        }
-                    </script>
-                </table>
-                <button type="submit">Park the Vehicle</button>
+                            function lineSelect(line){
+                                customer = line.querySelector('input[name="customer_id"]').value;
+                            }
+                        </script>
+                    </table><br>
+                    <table>
+                            <thead>
+                                <strong><legend>Parked Vehicles</legend></strong><br>
+                                <th>Vehicle</th>
+                                <th>Plate</th>
+                                <th>Parked Space</th>
+                            </thead>
+                            <tbody>
+                                <?php
+                                    $result = $conn->query("SELECT vehicles.*, parking.* FROM vehicles
+                                    JOIN parking ON vehicles.Vehicle_ID = parking.Vehicle_ID
+                                    WHERE parking.Parking_Departure_Date IS NULL;");
+                                    while($row_vehicle = mysqli_fetch_assoc($result)){
+                                        echo "<tr>";
+                                        echo "  <td>". $row_vehicle['Vehicle_Desc']. "</td>";
+                                        echo "  <td>". $row_vehicle['Vehicle_Plate']. "</td>";
+                                        echo "  <td>". $row_vehicle['Parking_Space_ID']. "</td>";
+                                        echo "</tr>";
+                                    }
+                                ?>
+                            </tbody>
+                    </table>
+                </div>
+                <button type="submit">Park Vehicle</button>
             </form>
         </div>
     <button><a href="index.php" style="text-decoration: none; color: black;">Back</a></button>
